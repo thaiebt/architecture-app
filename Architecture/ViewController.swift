36f901111b9947de0a48cfec2653b4dbe1728743
Thaina da Silva Ebert <thaiebert@gmail.com>
2023-01-02 14:56:37 -0300

    first commit

---
+++ Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture
+//
+//  Created by Thaina da Silva Ebert on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
